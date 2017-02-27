467e135e68e4c8fcfb1c7e1c1627026e86df09be
Javier Fernandez <jfernandez.developer@gmail.com>
2017-02-27 12:16:41 +0100

    Adding modules

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ProductListComponent } from './products/product-list-component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ 
+    AppComponent,
+    ProductListComponent ], // Adds the ProductListComponent, the name must be equal to the name that we put in the component ts file. Replacing snake-case to camel-case */ 
   bootstrap: [ AppComponent ] //Loads the AppComponent
 })
 export class AppModule { }
