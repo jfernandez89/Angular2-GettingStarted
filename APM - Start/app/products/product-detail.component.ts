193a9c29f85258d4ea9f31c52030a653bbbb2aa5
jfernandez89 <jfernandez.developer@gmail.com>
2017-03-02 09:58:51 +0100

    Routes with parameters &&  Navigation Back

--- APM - Start/app/products/product-detail.component.ts
+++ APM - Start/app/products/product-detail.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+
+//Needed to navigate with two params && Needed to navigates back
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
     //Selector property is only necesary if the it is a nested component.
     templateUrl: 'app/products/product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute, private _router: Router){
+        //Needed to use the ActivatedRoute.
+    }
     
+    ngOnInit(): void {
+        //Declares a variable which save information inside two objects
+        let id = +this._route.snapshot.params['id']; //Read from the ActivatedRoute the name that we put in RouterModule.forRoot 
+        this.pageTitle += `: ${id}`; //Set the value of the object into a String
+    }
+
+    /**
+     * This method use the Router to navigates back to the product list.
+     */
+    onBack(): void {
+        this._router.navigate(['/products']);
+    }
 }
